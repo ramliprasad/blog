93e77a52dbae7a663718c6ba55a37f92344510dc
Prasad Ramalingam <ramliprasad@gmail.com>
2014-03-19 16:12:26 +0000

    updating spring structure

---
+++ src/main/java/com/ramli/RantService.java
@@ -0,0 +1,18 @@
+package com.ramli;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RantService {
+private ArrayList<String> recentRants;
+
+public ArrayList<String> getRecentRants() {
+	return recentRants;
+}
+
+public void setRecentRants(ArrayList recentRants) {
+	this.recentRants = recentRants;
+}
+
+
+}
---
+++ src/main/java/com/ramli/Vehicle.java
@@ -0,0 +1,25 @@
+package com.ramli;
+
+public class Vehicle {
+	
+	private String state;
+	private String plateNumber;
+	
+	public String getState() {
+		return state;
+	}
+	
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	public String getPlateNumber() {
+		return plateNumber;
+	}
+	
+	public void setPlateNumber(String plateNumber) {
+		this.plateNumber = plateNumber;
+	}
+	
+
+}
